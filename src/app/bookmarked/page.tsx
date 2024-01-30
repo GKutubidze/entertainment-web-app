40e2cf1f70cc30c0c272ce2039d828966da5ed0e
Giorgi Kutubidze <ffs@gmail.com>
2024-01-30 15:19:14 +0400

    fix path

--- src/app/bookmarked/page.tsx
+++ src/app/bookmarked/page.tsx
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 import { useBookmarks } from '../(contexts)/BookmarksContext';
 import Search from '@/components/Search/Search';
 import { Recomended } from '@/components/Recomended/Recomended';
-
+import styles from "./page.module.css"
 const BookmarkedPage = () => {
   const [searchValue,setSearchValue]=useState<string>(" ");
 
   const { bookmarkedMovies, setBookmarkedMovies } = useBookmarks();
   console.log(bookmarkedMovies);
   return (
-    <div>
+    <div className={styles.main}>
       <Search setSearchValue={setSearchValue}/>
       <Recomended movieData={bookmarkedMovies} searchValue={searchValue} title="Bookmarked Movies"
       bookmarkedMovies={bookmarkedMovies}
